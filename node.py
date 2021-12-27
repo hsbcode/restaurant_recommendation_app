984a2e64cf97202acdec7349747a0fe8aeffc2a1
hsbcode <huncaseyfettin@gmail.com>
2021-12-27 16:46:28 +0300

    Create node.py
    Node class definition is added.

---
+++ node.py
@@ -0,0 +1,13 @@
+class Node:
+	def __init__(self):
+		self.value = value
+		self.next_node = next_node
+	
+	def set_next_node(self, next_node):
+		self.next_node = next_node
+
+	def get_next_node(self):
+		return self.next_node
+
+	def get_value:
+		return self.value
