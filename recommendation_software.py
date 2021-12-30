9b0ab54f5bdbd945a8600e2a1bce95254583ec2b
hsbcode <huncaseyfettin@gmail.com>
2021-12-30 14:36:47 +0300

    All data is added to linked lists

--- recommendation_software.py
+++ recommendation_software.py
@@ -2,7 +2,7 @@ import node
 import linked_list
 import restaurantData
 
-# Loading the restaurant and food data to a linked list
+# Loading the food data to a linked list
 def insert_food_type_to_linked_list():
     food_type_llist = linked_list.LinkedList()
     for type in restaurantData.types:
@@ -10,3 +10,17 @@ def insert_food_type_to_linked_list():
     return food_type_llist
 
 insert_food_type_to_linked_list()
+
+# Loading the restaurant data to a linked list
+def insert_resto_data_to_linked_list():
+    resto_llist = linked_list.LinkedList()
+    for type in restaurantData.types:
+        resto_type_llist = linked_list.LinkedList()
+        for resto in restaurantData.restaurant_data:
+            if resto[0] == type:
+                resto_type_llist.insert_beginning(resto)
+        resto_llist.insert_beginning(resto_type_llist)
+        resto_type_llist.insert_beginning(resto)
+    return resto_type_llist
+
+insert_resto_data_to_linked_list()
